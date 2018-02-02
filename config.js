524e6f5218d5391f3f459c0eb0a53a02977b6f49
kdesch <all.mail@web.de>
2018-02-02 14:24:49 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = {};
 config.cassandra = {};
-config.cassandra.contactPoints = [process.env.CASSANDRA_SERVICE_NAME];
+config.cassandra.contactPoints = [process.env.CASSANDRA_SERVICE_NAME, 'cassandra-db-lb.lb-cnap.bmwgroup.net', 'cassandra.db-lb.svc.cluster.local'];
 config.cassandra.keyspace = process.env.CASSANDRA_KEYSPACE;
 module.exports = config;
