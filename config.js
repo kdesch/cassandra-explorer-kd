368dc79a8a73b52c4b0b035727a31f32beddfa31
kdesch <all.mail@web.de>
2018-02-02 13:31:20 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = {};
 config.cassandra = {};
-config.cassandra.contactPoints = ['cassandra-db-lb.lb-cnap.bmwgroup.net'];
-config.cassandra.keyspace = 'animals';
+config.cassandra.contactPoints = [process.env.CASSANDRA_SERVICE_NAME];
+config.cassandra.keyspace = process.env.CASSANDRA_KEYSPACE;
 module.exports = config;
