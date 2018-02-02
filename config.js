5ace83c34bc7a345e1fa298b20fb1fd6d90cbc34
kdesch <all.mail@web.de>
2018-02-02 12:06:12 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 var config = {};
 config.cassandra = {};
-config.cassandra.contactPoints = ['localhost'];
+config.cassandra.contactPoints = ['cassandra.dl-lb.svc'];
+#config.cassandra.contactPoints = ['localhost'];
 config.cassandra.keyspace = 'system';
 module.exports = config;
